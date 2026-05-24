441bc69c8012cd6da423dde5a1dbf83c5dd74558
BadOmenX <2560349809@qq.com>
2026-05-24 15:58:54 +0800

    Delete baba-game director

--- baba-game/js/config.js
+++
@@ -1,5 +0,0 @@
-// Supabase 配置
-const SUPABASE_URL = 'https://hpgpuwozekxmkvhoduwd.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwZ3B1d296ZWt4bWt2aG9kdXdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MDExMjUsImV4cCI6MjA5NTE3NzEyNX0.gwu0eV5de73_AleWNJ0RxJdvuEyCbS9nvuqEbBD7I34';
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
